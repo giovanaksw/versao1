d2414efc1c879bd11249a2d2662db5ae012b7380
GIOVANA KOSLOWSKI <giovana_koslowski@estudante.sesisenai.org.br>
2023-11-23 09:46:01 -0300

    versao2

--- index.js
+++ index.js
@@ -68,13 +68,16 @@ app.get('/carrinho', (req,res)=>{
 })
 
 // ----- adm
-app.post('/cadastro', async(req,res)=>{
-    const nome = req.body.nome
-    const email = req.body.email
-    const telefone = req.body.telefone
-    const senha = req.body.senha
+app.post('/cadastrar_produto', async(req,res)=>{
+    const nome_produto = req.body.nome_produto
+    const cod_produto = Number(req.body.cod_produto)
+    const modelo = req.body.modelo
+    const marca = req.body.marca
+    const tipo = req.body.tipo
+    const qtde_estoque = Number(req.body.qtde_estoque)
+    const preco = Number(req.body.preco)
 
-    console.log(nome,email,telefone,senha)
+    console.log(nome_produto,cod_produto,modelo,marca,tipo,qtde_estoque,preco)
 
     bcrypt.hash(senha, 10, async (err,hash)=>{
         if(err){
@@ -83,47 +86,84 @@ app.post('/cadastro', async(req,res)=>{
             return
         }
         try{
-            await Usuario.create({nome: nome, email: email, telefone: telefone, senha: hash})
+            await Usuario.create({nome_produto: nome_produto, cod_produto: cod_produto, modelo: modelo, marca: marca, tipo: tipo, qtde_estoque: qtde_estoque, preco: preco,  senha: hash})
             console.log('\n')
             console.log('Senha criptografada')
             console.log('\n')
 
             log = true
 
-            const pesq = await Usuario.findOne({ raw: true, where:{ nome:nome, senha: hash}})
+            const pesq = await Usuario.findOne({ raw: true, where:{ nome:nome_produto, senha: hash}})
             console.log(pesq)
 
             res.render('home', {log})
         }catch(error){
-            console.error('Erro ao criar a senha',error)
+            console.error('Erro ao criar a senha'+ error)
             res.render('home', {log})
         }
     })
 })
-app.get('/cadastro', (req,res)=>{
-    res.render('cadastro', {log})
+app.get('/cadastrar_produto', (req,res)=>{
+    res.render('cadastrar_produto', {log})
 })
 
-
-app.post('/editarProduto', async (req,res)=>{
-    const nome = req.body.nome
-    const tamanho = Number(req.body.tamanho)
+app.post('/atualizar_produto', async (req,res)=>{
+    //config atuais
+    const nome_produto = req.body.nome_produto
+    const cod_produto = req.body.cod_produto
+    const qtde_estoque = req.body.qtde_estoque
     const cor = req.body.cor
-    const tipo = req.body.tipo
-    const quantidadeEstoque = Number(req.body.quantidadeEstoque)
-    const precoUnitario = Number(req.body.precoUnitario)
-    const descricao = req.body.descricao
-    console.log(nome,tamanho, cor, tipo, quantidadeEstoque, precoUnitario, descricao)
+    const tamanho = req.body.tamanho
+    const preco = req.body.preco
+    //novas config
+    const novo_id = req.body.novo_id
+    const novo_nome_produto = req.body.novo_nome_produto
+    const novo_cod_produto = req.body.novo_cod_produto
+    const novo_qtde_estoque = req.body.novo_qtde_estoque
+    const novo_cor = req.body.novo_cor
+    const novo_tamanho = req.body.novo_tamanho
+    const novo_preco = req.body.novo_preco
+
+    const pesq = await Produto.findOne({raw:true, where:{ nome_produto:nome_produto, cod_produto:cod_produto, qtde_estoque:qtde_estoque, cor:cor, tamanho:tamanho, preco:preco}})
+
+    const dados = {
+        nome_produto: novo_nome_produto,
+        cod_produto: novo_cod_produto,
+        qtde_estoque: novo_qtde_estoque,
+        cor: novo_cor,
+        tamanho: novo_tamanho,
+        preco: novo_preco,
+    }
+
+    let msg = 'Dados não encontrados'
+    let msg2 = 'Produto Atualizado!'
+    log = true
+
+    if(pesq==null){
+        res.render('atualizar_produto', {log, adm, nomeAdm, msg})
+    }else{
+        await Produto.update(dados, {where:{nome_produto:pesq.nome_produto, cod_produto:pesq.cod_produto, qtde_estoque:pesq.qtde_estoque, cor:pesq.cor, tamanho:pesq.tamanho, preco:pesq.preco}})
+        res.render('atualizar_produto', {log, adm, nomeAdm, msg2})
+    }
+})
+
+app.get('/atualizar_produto', (req,res)=>{
+    res.render('atualizar_produto', {log, adm, nomeAdm})
+})
+
+app.post('/consulta_produto', async (req, res)=>{
+    const nome_produto = req.body.nome
+    console.log(nome_produto)
     const dados = await Produto.findOne({raw:true, where: {nome:nome_produto}})
     console.log(dados)
-    res.redirect('/editarProduto')
-
-})
-app.get('/editarProduto', (req,res)=>{
-    res.render('editarProduto', {log, usuario, tipoUsuario})
+    res.render('editarProduto',{log, usuario, tipoUsuario, valor:dados} )
 })
 
-
+app.get('/listar_produto', async (req,res)=>{
+    const dados = await Produto.findAll({raw:true})
+    console.log(dados)
+    res.render('listar_produto', {log, usuario, tipoUsuario, valores:dados})
+})
 
 // ------
 conn.sync().then(()=>{
